9812d7ad5765cd91b8140a72cde40f6aee8e8575
Fernando Fuentes Perez <ffuentep@everis.com>
2021-10-07 14:52:25 +0200

    feat: Added read properties from config server

--- loans/src/main/java/com/fernandofuentesfullstack/loans/controller/LoanController.java
+++ loans/src/main/java/com/fernandofuentesfullstack/loans/controller/LoanController.java
@@ -1,9 +1,15 @@
 package com.fernandofuentesfullstack.loans.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fernandofuentesfullstack.loans.config.LoansServiceConfig;
 import com.fernandofuentesfullstack.loans.model.Customer;
 import com.fernandofuentesfullstack.loans.model.Loan;
+import com.fernandofuentesfullstack.loans.model.Properties;
 import com.fernandofuentesfullstack.loans.repository.LoanRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,16 +22,25 @@ public class LoanController {
     @Autowired
     private LoanRepository loanRepository;
 
+    @Autowired
+    LoansServiceConfig loansServiceConfig;
+
     @PostMapping("/myLoans")
     public List<Loan> getLoanDetails(@RequestBody Customer customer) {
-
         List<Loan> loans = loanRepository.findByCustomerIdOrderByStartDtDesc(customer.getCustomerId());
-
         if (loans != null) {
             return loans;
         } else {
             return null;
         }
+    }
 
+    @GetMapping("/loans/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(loansServiceConfig.getMsg(), loansServiceConfig.getBuildVersion(),
+                loansServiceConfig.getMailDetails(), loansServiceConfig.getActiveBranches());
+        String jsonStr = ow.writeValueAsString(properties);
+        return jsonStr;
     }
 }
