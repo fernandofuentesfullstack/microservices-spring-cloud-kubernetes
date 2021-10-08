20481a79b53b802fe4ad7cf618f2fad7b7840de0
Fernando Fuentes Perez <ffuentep@everis.com>
2021-10-08 11:08:10 +0200

    feat: Added refresh properties

--- accounts/src/main/java/com/fernandofuentesfullstack/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/fernandofuentesfullstack/accounts/AccountsApplication.java
@@ -3,11 +3,13 @@ package com.fernandofuentesfullstack.accounts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@RefreshScope
 @ComponentScans({ @ComponentScan("com.fernandofuentesfullstack.accounts.controller")})
 @EnableJpaRepositories("com.fernandofuentesfullstack.accounts.repository")
 @EntityScan("com.fernandofuentesfullstack.accounts.model")
