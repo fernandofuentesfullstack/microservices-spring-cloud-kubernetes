14e52e1dc6aaa40f653bcf0a371bf381b31e6b2d
fernandofuentesfullstack <fernandofuentesfullstack@gmail.com>
2021-11-07 20:53:49 +0100

    Add: Added custom routes

--- gatewayserver/src/main/java/com/fernandofuentesfullstack/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/fernandofuentesfullstack/gatewayserver/GatewayserverApplication.java
@@ -2,7 +2,12 @@ package com.fernandofuentesfullstack.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -10,6 +15,26 @@ public class GatewayserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/fernandofuentes/accounts/**")
+						.filters(f -> f.rewritePath("/fernandofuentes/accounts/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time",new Date().toString()))
+						.uri("lb://ACCOUNTS")).
+				route(p -> p
+						.path("/fernandofuentes/loans/**")
+						.filters(f -> f.rewritePath("/fernandofuentes/loans/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time",new Date().toString()))
+						.uri("lb://LOANS")).
+				route(p -> p
+						.path("/fernandofuentes/cards/**")
+						.filters(f -> f.rewritePath("/fernandofuentes/cards/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time",new Date().toString()))
+						.uri("lb://CARDS")).build();
 	}
 
 }
